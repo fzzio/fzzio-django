326eaf5733c491359853a450da95545fd1643547
Fabricio Orrala Parrales <fabricio@cajanegra.com.ec>
2016-07-09 14:44:18 -0500

    primer commit blog django

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/fzzio/Documentos/Aprendiendo/tallerPython/djangogirls/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
